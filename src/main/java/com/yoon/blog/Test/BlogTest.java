809cee222647ae83da64e452dcaa74269ff841cb
yoonwongoo <yunyun26@naver.com>
2021-07-09 22:08:08 +0900

    환경설정세팅

---
+++ src/main/java/com/yoon/blog/Test/BlogTest.java
@@ -0,0 +1,17 @@
+package com.yoon.blog.Test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogTest {
+
+	
+	@GetMapping("/test")
+	public String Hello() {
+	
+		return"안녕스트링부트";
+		
+	}
+}
---
+++ src/main/java/com/yoon/blog/YoonBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoonBlogApplication.class, args);
+	}
+
+}
